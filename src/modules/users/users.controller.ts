a9c32571c22d8f55cdcb4ebbb2ed1ef31a09b9d3
momenashrafff <momen.elkhouli@student.giu-uni.de>
2025-04-19 18:07:21 +0200

    added get user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Put, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -7,10 +7,47 @@ import { GetUser } from 'src/common/decorators/getUser.decorator';
 
 @ApiTags('User')
 @Controller('user')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @UseGuards(JwtAuthGuard)
+  // make getprofile endpoint
+  @Get('')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get user profile' })
+  @ApiResponse({
+    status: 200,
+    description: 'User profile retrieved successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
+  async getProfile(@GetUser('userId') userId: string) {
+    const user = await this.usersService.findById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }    
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      isEmailVerified: user.isEmailVerified,
+      gender: user.gender,
+      dob: user.dob,
+      height: user.height,
+      weight: user.weight,
+      activityLevel: user.activityLevel,
+      goal: user.goal,
+      dailyCalories: user.dailyCalories,
+      caloriesLeft: user.caloriesLeft,
+      memberSince: user.created_at,
+    };
+  }
+  
   @Put('')
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Update user profile' })
